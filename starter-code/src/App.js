b6340f52a7ad59dee9ed269e006b17bfafe12268
Chloe <chloe@Chloes-MacBook-Air.local>
2019-12-04 10:38:25 +0100

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {Switch, Route, Link} from 'react-router-dom';
+import AllBeers from './components/AllBeers';
+import RandomBeer from './components/RandomBeer';
+import NewBeer from './components/NewBeer';
+
+
+
+import axios from 'axios';
+
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      theBeers: [],
+      ready: false
+    }
+  }
+
+
+  fetchTheBeers(){
+      axios.get('https://ih-beer-api.herokuapp.com/beers')
+      .then(response => {
+        this.setState({
+          // get just the latest created 20 beers
+          theBeers: response.data.reverse().slice(0,20),
+          ready: true
+        })
+      })
+      .catch(err => console.log(err))   
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Link to="/all-beers">See All Beers</Link>
+        <Link to="/random">Random Beer</Link>
+        <Link to="/new">Add New</Link>
+
+        {this.fetchTheBeers()}
+        
+        <Switch>
+          <Route exact path="/all-beers" render={props =>
+            <AllBeers
+              {...props}
+              beers={this.state.theBeers}
+              ready={this.state.ready}
+              beersCount={this.state.theBeers.length}
+            /> 
+          } />
+
+          <Route exact path="/random" render={props =>
+            <RandomBeer
+              {...props}
+              beers={this.state.theBeers}
+              ready={this.state.ready}
+            /> 
+          } />
+
+          <Route exact path="/new" component={NewBeer} />
+        </Switch>
       </div>
     );
   }
